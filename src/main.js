0df5c3efaa4a5c41bcdba94d4049232606a356b5
Nelly <mollocondonelly@gmail.com>
2022-11-13 10:38:59 -0500

    configuracion del entorno de desarrollo

---
+++ src/main.js
@@ -0,0 +1,2 @@
+console.log('Hola');
+const API_URL = `https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+//Crea por favor un arhivo secrets.js con tu API KEY
+const API_KEY = '';
